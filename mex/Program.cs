71965007e9c6cbd584479176ab11b881a028c4c6
s.naiden <s.naiden@rlt.ru>
2021-03-11 11:15:57 +0300

    initial commit

---
+++ mex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
