13188f55b64d00cb0db50cad28a35486641c3bfc
s.naiden <s.naiden@rlt.ru>
2021-03-11 17:23:49 +0300

    - Заготовка

---
+++ mex/MexDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mex
+{
+    public class MexDB
+    {
+        #region Поля
+
+        /// <summary>Кэш ID</summary>
+        public readonly List<uint> cache = new List<uint>();
+
+        #endregion
+
+        public MexDB()
+        {
+            cache.Add(1);
+            cache.Add(3);
+        }
+
+        /// <summary>Получить следующий ID</summary>
+        public uint GetNext()
+        {
+            return 0;
+        }
+
+        /// <summary>Перестроить базу</summary>
+        public void Rebuild(uint seed)
+        {
+            for (var i = 0; i < cache.Count; i++)
+                cache[i] ^= seed;
+        }
+
+    }
+}
--- mex/Program.cs
+++ mex/Program.cs
@@ -6,7 +6,37 @@ namespace mex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mex = new MexDB();
+
+            var key = "";
+            uint seed = 0;
+
+            Console.WriteLine("Новый ID (Enter)");
+            Console.WriteLine("Зашифровать базу (цифра)");
+            Console.WriteLine("Выход (exit)");
+
+            while (true)
+            {
+                Console.WriteLine("Список ID:");
+                Console.WriteLine(string.Join(", ", mex.cache));
+
+                key = Console.ReadLine();
+                if (key == "exit")
+                    break;
+
+                // Если у нас цифра - шифруем базу
+                if (uint.TryParse(key, out seed))
+                {
+                    mex.Rebuild(seed);
+                    continue;
+                }
+
+                var id = mex.GetNext();
+                Console.WriteLine($"Добавили ID: {id}");
+            }
+
+            Console.WriteLine("Any key to exit....");
+            Console.ReadKey();
         }
     }
 }
